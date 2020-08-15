7fdd9fe1bff3f0b33a72203f11a8cfc58d6a9d2b
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-08-16 00:49:12 +0900

    label json file5

--- slowfast/visualization/video_visualizer.py
+++ slowfast/visualization/video_visualizer.py
@@ -670,5 +670,6 @@ class VideoVisualizer:
         print(len(thres_array))
         print(thres_array)
         print(common_class_ids)
+        thres_array.append(0.3)
         thres_array[common_class_ids] = self.thres
         self.thres = torch.from_numpy(thres_array)
