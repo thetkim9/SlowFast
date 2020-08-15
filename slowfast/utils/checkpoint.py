410dfef5d35a8d7abe1b1095e691aa9bf33ae7f0
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-08-16 02:11:19 +0900

    model run error6

--- slowfast/utils/checkpoint.py
+++ slowfast/utils/checkpoint.py
@@ -264,7 +264,10 @@ def load_checkpoint(
         with PathManager.open(path_to_checkpoint, "rb") as f:
             print(f)
             device = torch.device('cpu')
-            checkpoint = torch.load(f, map_location=device, encoding='latin1')
+            #checkpoint = torch.load(f, map_location=device, encoding='latin1')
+            import pickle
+            obj = f.read()
+            weights = pickle.loads(obj, encoding='latin1')
         model_state_dict_3d = (
             model.module.state_dict() if data_parallel else model.state_dict()
         )
