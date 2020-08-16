ae0ac1a87bc889dd22530d660e2d3b346c5cb26f
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-08-17 00:01:13 +0900

    different input

--- tools/run_net.py
+++ tools/run_net.py
@@ -19,11 +19,11 @@ def main():
     cfg = load_config(args)
     #cfg.DEMO.WEBCAM = 0
     cfg.DEMO.WEBCAM = -1
-    cfg.DEMO.INPUT_VIDEO = "demo_test/demo_in.mp4"
+    cfg.DEMO.INPUT_VIDEO = "demo_test/sample_in.mp4"
     cfg.NUM_GPUS = 1
     cfg.TRAIN.ENABLE = False
     cfg.TEST.ENABLE = False
-    cfg.DEMO.OUTPUT_FILE = "demo_test/demo_out.mp4"
+    cfg.DEMO.OUTPUT_FILE = "demo_test/sample_out.mp4"
     cfg.DEMO.ENABLE = True
     # Perform training.
     if cfg.TRAIN.ENABLE:
